3cb775bd14bdeb1f0ef6644a815fdec4955c28f6
Emanuele Dinaro <ema.dinaro@gmail.com>
2025-06-19 10:52:20 +0200

    feat: add Actress type alias extending Person with additional properties

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,3 @@
-// Milestone 1
-// Crea un type alias Person per rappresentare una persona generica.
-
-// Il tipo deve includere le seguenti proprietà:
-
-// id: numero identificativo, non modificabile
-// name: nome completo, stringa non modificabile
-// birth_year: anno di nascita, numero
-// death_year: anno di morte, numero opzionale
-// biography: breve biografia, stringa
-// image: URL dell'immagine, stringa
-
 type Person = {
   readonly id: number;
   readonly name: string;
@@ -17,4 +5,29 @@ type Person = {
   death_year?: number;
   biography: string;
   image: string;
+};
+
+// Milestone 2
+// Crea un type alias Actress che oltre a tutte le proprietà di Person, aggiunge le seguenti proprietà:
+
+// most_famous_movies: una tuple di 3 stringhe
+// awards: una stringa
+// nationality: una stringa tra un insieme definito di valori.
+// Le nazionalità accettate sono: American, British, Australian, Israeli-American, South African, French, Indian, Israeli, Spanish, South Korean, Chinese.
+
+type Actress = Person & {
+  most_famous_movies: [string, string, string];
+  awards: string;
+  nationality:
+    | "American"
+    | "British"
+    | "Australian"
+    | "Israeli-American"
+    | "South African"
+    | "French"
+    | "Indian"
+    | "Israeli"
+    | "Spanish"
+    | "South Korean"
+    | "Chinese";
 };
